6f841b6a9e86df71e4d7975be591105a6f8c2187
Nitesh_Devkota <niteshdevkota2016@gmail.com>
2021-05-13 21:31:01 +0545

    initialized

---
+++ server/app.js
@@ -0,0 +1,47 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+
+const {
+    response
+} = require('express');
+const express = require('express');
+const app = express();
+
+dotenv.config({path: './config.env'});
+require('./db/conn.js')
+const PORT = process.env.PORT;
+
+// mongodb user = nitzech pw = 123ni.te
+
+//mongodb+srv://nitzech:<password>@cluster0.mb1mx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+
+
+const middleware = (req, res, next) => {
+    console.log('Here is my middleware');
+    next();
+}
+
+
+app.get('/', (req, res) => {
+    res.send(`Hello world from the backend server`)
+});
+
+app.get('/home', (req, res) => {
+    res.send(`Hello world from the Home server`)
+});
+
+app.get('/about', middleware, (req, res) => {
+    res.send(`Hello world from the About server`);
+    console.log('Here is my about');
+
+});
+
+app.get('/services', (req, res) => {
+    res.send(`Hello world from the services server`)
+});
+
+app.listen(PORT, () => {
+    console.log(` running at ${PORT}`)
+})
