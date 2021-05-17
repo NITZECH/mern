8a3b1edbab8ef6ef492c91934697f1ed2149c84a
Nitesh_Devkota <niteshdevkota2016@gmail.com>
2021-05-17 18:05:49 +0545

    completion of contact page frontend without logic

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
+import { NavLink } from 'react-router-dom'
 
 const Contact = () => {
 return (
@@ -10,7 +11,7 @@ return (
                 <div className="col-lg-10 offset-lg-1 d-flex justify-content-between">
                     {/* phone number */}
                     <div className="contact_info_item d-flex align-items-center justify-content-center">
-                        <img src="" alt="" />
+                    <img src="https://img.icons8.com/android/64/000000/phone.png" alt='phone' /> 
                         <div className="contact_info_content">
                             <div className="contact_info_title">
                                 Phone
@@ -22,7 +23,7 @@ return (
                     </div>
                     {/* email */}
                     <div className="contact_info_item d-flex align-items-center justify-content-center">
-                        <img src="" alt="" />
+                    <img src="https://img.icons8.com/pastel-glyph/64/000000/email--v3.png"  alt='email' />
                         <div className="contact_info_content">
                             <div className="contact_info_title">
                                 Email
@@ -34,7 +35,7 @@ return (
                     </div>
                     {/* address */}
                     <div className="contact_info_item d-flex align-items-center justify-content-center">
-                        <img src="" alt="" />
+                    <img src="https://img.icons8.com/carbon-copy/64/000000/address.png" alt='adresss' />
                         <div className="contact_info_content">
                             <div className="contact_info_title">
                                 Address
@@ -47,7 +48,42 @@ return (
                 </div>
             </div>
         {/* contact form  */}
-        
+        <div className="contact_form">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-10 offset-lg-1 ">
+                     <div className="contact_form_container py-5">
+                         <div className="contact_form_title justify-content-between ">
+Get in Touch
+                         </div>
+                         <form id='contact_form'>
+                             <div className="contact_form_name d-flex justify-content-between align-items-center  py-3">
+<input type='text' id='contact_form_name'
+ className='contact_form_name input_field'
+placeholder='Your Name' required='true' />
+
+<input type='email' id='contact_form_email' 
+className='contact_form_email input_field'
+placeholder='Your Email' required='true' />
+
+<input type='number' id='contact_form_phone' 
+className='contact_form_phone input_field'
+placeholder='Your Phone Number' required='true' />
+                             </div>
+<div className="contact_form_text">
+    <textarea placeholder='Your Message' className='text_field contact_form_message' id='' cols='30' rows='10'>
+        </textarea> 
+</div>
+<div className="contact_form_button">
+    <button className="btn btn-lg btn-outline-secondary" type='submit'> Send Message</button>
+</div>
+
+                         </form>
+                    </div>   
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     </div>
 </>
