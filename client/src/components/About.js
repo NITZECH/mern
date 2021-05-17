201e4f1b376b510b84532bda556ea22942eabb15
Nitesh_Devkota <niteshdevkota2016@gmail.com>
2021-05-17 22:12:30 +0545

    added some functionality to the about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,52 +1,148 @@
 import React from 'react'
 
 const About = () => {
-    return (
-        <>
-           <div className="container emp-profile">
-               <form>
-                  <div className="row">
-                      <div className="col-md-4">
-                          <img src="https://scontent.fpkr2-1.fna.fbcdn.net/v/t1.6435-1/c122.257.968.968a/s240x240/174440514_2816772958540496_4707193922734414668_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=7206a8&_nc_ohc=E1vZCxyCsQsAX8s6MeK&_nc_ht=scontent.fpkr2-1.fna&tp=28&oh=5b1b56a8d5c8a9aa95e924282e04cb36&oe=60C8BFF7" alt="" />
-                      </div>
-                      <div className="col-md-6">
-<div class="profile-head">
-    <h5>Vinod thapa</h5>
-    <h6>Web developer</h6>
-    <p className='profile-rating mt-3 mb-5'>RANKINGS: <span> 1/ 10</span> </p>
+return (
+<>
+    <div className="container emp-profile">
+        <form>
+            <div className="row">
+                <div className="col-md-4 profile-img">
+                    <img src="https://scontent.fpkr2-1.fna.fbcdn.net/v/t1.6435-1/c122.257.968.968a/s240x240/174440514_2816772958540496_4707193922734414668_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=7206a8&_nc_ohc=E1vZCxyCsQsAX8s6MeK&_nc_ht=scontent.fpkr2-1.fna&tp=28&oh=5b1b56a8d5c8a9aa95e924282e04cb36&oe=60C8BFF7"
+                        alt="" />
+                </div>
+                <div className="col-md-6">
+                    <div class="profile-head">
+                        <h5>Vinod thapa</h5>
+                        <h6>Web developer</h6>
+                        <p className='profile-rating mt-3 mb-5'>RANKINGS: <span> 1/ 10</span> </p>
 
-<ul className="nav nav-tabs" role='tablist'>
-    <li className="nav-item">
-        <a href="#home" className="nav-link active" id='home-tab' data-toggle='tab' role='tab'  >Active</a>
-    </li>
-    <li className="nav-item">
-    <a href="#about" className="nav-link active" id='profile-tab' data-toggle='tab' role='tab'  >Timeline</a>
-    </li>
-</ul>
+                        <ul className="nav nav-tabs" role='tablist'>
+                            <li className="nav-item">
+                                <a href="#home" className="nav-link active" id='home-tab' data-toggle='tab' aria-controls='tab'
+                                    role='tab'>Active</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="#profile" className="nav-link " id='profile-tab' data-toggle='tab' aria-controls='tab'
+                                    role='tab'>Timeline</a>
+                            </li>
+                        </ul>
 
 
-</div>
+                    </div>
 
-                      </div>
-                      <div className="col-md-2">
-                          <input type="text" name='btnAddMore' className="profile-edit-btn" value='Edit Profile' />
-                      </div>
-                      <div className="row">
-                          {/* left side url */}
-<div className="col-md-4">
-    <div className="profile-work">
-        <p>Work Link</p>
+                </div>
+                <div className="col-md-2">
+                    <input type="text" name='btnAddMore' className="profile-edit-btn" value='Edit Profile' />
+                </div>
+                <div className="row">
+                    {/* left side url */}
+                    <div className="col-md-4">
+                        <div className="profile-work">
+                            <p>Work Link</p>
+                            <a href="https://github.com/NITZECH" target='_nitesh'>GitHub</a> <br />
+                            <a href="https://github.com/NITZECH" target='_nitesh'>GitHub</a> <br />
+                            <a href="https://github.com/NITZECH" target='_nitesh'>GitHub</a> <br />
+                            <a href="https://github.com/NITZECH" target='_nitesh'>GitHub</a> <br />
+                            <a href="https://github.com/NITZECH" target='_nitesh'>GitHub</a> <br />
+                        </div>
+                    </div>
+                    {/* right side data toggle */}
+                    <div className="col-md-8 pl-5 about-info">
+                        <div className="tab-content profile-tab" id='myTabContent'></div>
+                        <div className="tab-pane fade show active collapse"  id='home' role='tabpanel'>
 
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <label>User ID</label>
+                                </div>
+                                <div className="col-md-6">
+                                    <p>65464646</p>
+                                </div>
+                            </div>
+
+                            <div className="row mt-3">
+                                <div className="col-md-6">
+                                    <label>Name</label>
+                                </div>
+                                <div className="col-md-6">
+                                    <p>Nitesh Devkota</p>
+                                </div>
+                            </div>
+
+                            <div className="row mt-3">
+                                <div className="col-md-6">
+                                    <label>Name</label>
+                                </div>
+                                <div className="col-md-6">
+                                    <p>Nitesh Devkota</p>
+                                </div>
+                            </div>
+                            <div className="row mt-3">
+                                <div className="col-md-6">
+                                    <label>Name</label>
+                                </div>
+                                <div className="col-md-6">
+                                    <p>Nitesh Devkota</p>
+                                </div>
+                            </div>
+                            <div className="row mt-3">
+                                <div className="col-md-6">
+                                    <label>Name</label>
+                                </div>
+                                <div className="col-md-6">
+                                    <p>Nitesh Devkota</p>
+                                </div>
+                            </div>
+                       </div>
+
+                       {/* profile tab  */}
+
+                       <div className="tab-pane fade " id='profile' role='tabpanel'   aria-labelledby='profile-tab'>
+
+<div className="row">
+    <div className="col-md-6">
+        <label>Experience</label>
+    </div>
+    <div className="col-md-6">
+        <p>Intermediate</p>
     </div>
 </div>
-{/* right side data toggle  */}
+
+<div className="row mt-3">
+    <div className="col-md-6">
+        <label>Hourly Rate</label>
+    </div>
+    <div className="col-md-6">
+        <p>$35/hr</p>
+    </div>
+</div>
+
+<div className="row mt-3">
+    <div className="col-md-6">
+        <label>Total Projects</label>
+    </div>
+    <div className="col-md-6">
+        <p>100</p>
+    </div>
+</div>
+<div className="row mt-3">
+    <div className="col-md-6">
+        <label>English Level</label>
+    </div>
+    <div className="col-md-6">
+        <p>Fluent</p>
+    </div>
+</div>
+</div>
 
 
-                      </div>
-                      </div> 
-                   </form> </div>     
-        </>
-    )
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</>
+)
 }
 
 export default About
