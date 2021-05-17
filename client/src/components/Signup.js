3b69efd2e4f8780b58ba4c5e7ec6971c61400625
Nitesh_Devkota <niteshdevkota2016@gmail.com>
2021-05-17 15:35:50 +0545

    completion of registration page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,40 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 const Signup = () => {
 return (
 <>
-    {/* <section className="signup">
-        <div className="container mt-5">
-            <div className="signup-content">
-                <div className="signup-form">
-                    <h2 className="form-title">Sign Up</h2>
-                    <form action="" className="register-form" id='register-form'>
-                        <form>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1"
-                                    aria-describedby="emailHelp" placeholder="Enter email" />
-                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with
-                                    anyone else.</small>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1"
-                                    placeholder="Password" />
-                            </div>
-                            <div className="form-check">
-                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </form>
-
-                    </form>
-                </div>
-            </div>
-        </div>
-
-    </section> */}
-
-
+   
     <section className="signup">
         <div className="container mt-5 mx-auto">
             <div className="signup-content">
