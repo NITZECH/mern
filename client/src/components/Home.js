98c7fddc2cccc504f8d8e4b1951dd1bf1dd8ac21
Nitesh_Devkota <niteshdevkota2016@gmail.com>
2021-05-17 18:22:23 +0545

    finishing up with homepage which is simple as it is not a complex project

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <p> WLcoem </p>
-            
-        </div>
+        <>
+          <div className="home-page">
+              <div className="home-div">
+                  <h2 className="pt-5">
+                    Welcome
+                  </h2>
+                  <h1>This is a simple MERN project showing Registration, Login with connection to React in Frontend and MongoDB in backend. </h1>
+                  </div> </div> 
+        </>
     )
 }
 
