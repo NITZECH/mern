f6579c0ad485c4e1c431469cc7ee707e7774a6cf
Nitesh_Devkota <niteshdevkota2016@gmail.com>
2021-05-18 11:21:56 +0545

    added user authentication

--- server/middleware/autheticate.js
+++ server/middleware/autheticate.js
@@ -6,7 +6,17 @@ const User = require('../model/userSchema');
 const autheticate = async  (res, req, next) => {
 try{
 const token = req.cookies.token;
-const verifyToken =
+const verifyToken = jwt.verify( token, process.env.SECRET_KEY);
+
+const rootUser = await User.findOne({ _id: verifyToken._id , "tokens : token ": token});
+
+if (!rootUser) { throw new Error('User Not Found')}
+
+req.token = token;
+req.rootUser = rootUser;
+req.userID = rootUser._id;
+next();
+
 }catch(err){
     res.status(401).send("Unauthorized: NO TOKEN PROVIDED")
     console.log(err);
